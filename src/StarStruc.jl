094bc1e64ccd3625407e8bde2cea153417a15ae7
Asher Wasserman <adwasser@ucsc.edu>
2016-10-13 14:56:16 -0700

    beta, rho, mu, and constants

--- src/StarStruc.jl
+++ src/StarStruc.jl
@@ -1,7 +1,65 @@
+#=
+All units are cgs.
+=#
+
 module StarStruc
 
-function test()
-    println("Hello, world!")
+# provides c0, m_H, k_B, sigma_sb, a_rad
+include("constants.jl")
+
+function mu(X, Y)
+    #=
+    Calculates the dimensionless mean molecular weight for a fully ionized gas.
+    
+    Parameters
+    ----------
+    X : hydrogen mass fraction
+    Y : helium mass fraction
+    
+    Returns
+    -------
+    mu : mean molecular weight
+    =#
+    return 2 / (1 + 3 * X + 0.5 * Y)
 end
 
+function beta(Pgas, T)
+    #=
+    Calculates the ratio between gas pressure and total pressure.
+
+    Parameters
+    ----------
+    Pgas : gas pressure in dyne cm^-2
+    T : temperature in K
+
+    Returns
+    -------
+    beta : Pgas / P
+    =#
+    Prad = a_rad * T^4 / 3
+    return Pgas / (Pgas + Prad)
 end
+    
+function rho(Pgas, T, X, Y)
+    #=
+    Calculates the density of an ideal fully ionized gas.
+
+    Parameters
+    ----------
+    Pgas : gas pressure in dyne cm^-2
+    T : temperature in K
+    X : hydrogen mass fraction
+    Y : helium mass fraction
+
+    Returns
+    -------
+    rho : density in g cm^-3
+    =#
+    Prad = a_rad * T^4 / 3
+    P = Pgas + Prad
+    return P * m_H * mu(X, Y) / (k_B * T)
+end
+
+
+end # module
+
---
+++ src/constants.jl
@@ -0,0 +1,14 @@
+# speed of light in a vacuum in cm s^-1
+c0 = 2.99792458e10
+    
+# atomic mass unit in g
+m_H = 1.660539040e-30
+
+# Boltzmann constant in erg K^-1
+k_B = 1.38064852e-16
+
+# Stefan-Boltzmann constant in erg cm^-2 s^-1 K^-4
+sigma_sb = 5.6704e-5
+
+# radiation density constant in erg cm^-3 K^-4
+a_rad = 4 * sigma_sb / c0
